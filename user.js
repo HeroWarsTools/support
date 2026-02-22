506c1135f50bdfad4b2afbad16672a544c127963
HeroWarsTools <johnnymagiaherowars@gmail.com>
2026-02-22 14:12:59 +0100

    Create user.js for browser preferences
    Add user.js with preferences for external protocol handling and popup management.

---
+++ user.js
@@ -0,0 +1,14 @@
+// Digita Ctrl+S per scaricare il file da github
+// Impostazioni per la gestione dei protocolli esterni
+user_pref("network.protocol-handler.expose-all", true);
+user_pref("network.protocol-handler.warn-external.ext+container", false);
+user_pref("network.protocol-handler.external.ext+container", true);
+
+// Disabilita il blocco dei popup (permette l'apertura di finestre durante il caricamento)
+user_pref("dom.disable_open_during_load", false);
+
+
+// istruzioni : digita about:support per sapere dov'è la Cartella del profilo (o "Profile Folder")
+// Copia il file: Chiudi completamente Firefox e trascina il file user.js
+
+// 
